5fb2e66cbca8612b7c2222c891b44eef0bd983ba
Batool Fahmi <batool.fahmi.webd24jon@edu.tucsweden.se>
2026-02-18 21:05:18 +0100

    Fix header and footer layout

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -21,23 +21,14 @@ function Footer() {
           </a>
         </div>
 
-        {/* HAMBURGER ICON (gamla versionen) */}
-        <div className="footer-menu-icon">
-          <span></span>
-          <span></span>
-          <span></span>
-        </div>
-
-        {/* FOOTER MENU (gamla versionen) */}
-        <nav className="footer-menu">
-          <ul>
-            <li><a href="#home">Home</a></li>
-            <li><a href="#about">About</a></li>
-            <li><a href="#blog">Blog</a></li>
-            <li><a href="#expertise">Expertise</a></li>
-            <li><a href="#projects">Projects</a></li>
-            <li><a href="#contact">Contact</a></li>
-          </ul>
+        {/* MENU */}
+        <nav className="footer-nav">
+          <a href="#home">Home</a>
+          <a href="#about">About</a>
+          <a href="#blog">Blog</a>
+          <a href="#expertise">Expertise</a>
+          <a href="#projects">Projects</a>
+          <a href="#contact">Contact</a>
         </nav>
 
       </div>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,34 +1,26 @@
 function Header() {
   return (
-    <header className="header">
-      <div className="header-inner">
+ <header className="header">
+  <div className="header-inner">
 
-        {/* LOGO */}
-        <div className="logo">
-          <a href="#home">Batool</a>
-        </div>
+    <div className="logo">
+      <a href="#home">Batools Portfolio</a>
+    </div>
 
-        {/* NAVIGATION */}
-        <nav className="nav">
-          <ul>
-            <li><a href="#home">Home</a></li>
-            <li><a href="#about">About</a></li>
-            <li><a href="#blog">Blog</a></li>
-            <li><a href="#expertise">Expertise</a></li>
-            <li><a href="#projects">Projects</a></li>
-            <li><a href="#contact">Contact</a></li>
-          </ul>
-        </nav>
+    <nav className="nav">
+      <ul>
+        <li><a href="#home">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#blog">Blog</a></li>
+        <li><a href="#expertise">Expertise</a></li>
+        <li><a href="#projects">Projects</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+    </nav>
 
-        {/* HAMBURGER ICON */}
-        <div className="menu-icon">
-          <span></span>
-          <span></span>
-          <span></span>
-        </div>
+  </div>
+</header>
 
-      </div>
-    </header>
   );
 }
 
