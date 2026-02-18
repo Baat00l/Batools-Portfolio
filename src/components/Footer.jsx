e55c85506d80324edad1df6579cd8185b301e57c
Batool Fahmi <batool.fahmi.webd24jon@edu.tucsweden.se>
2026-02-18 19:59:09 +0100

    Add full mobile-responsive layout

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,9 +6,9 @@ function Footer() {
         <p className="footer-copy">© 2026 Batool Fahmi. Alla rättigheter förbehållna.</p>
 
         <div className="footer-center">
-         <a href="https://www.instagram.com/b4h.mi/" target="_blank" className="social-icon">
-  <i className="fa-brands fa-instagram"></i>
-</a>
+          <a href="https://www.instagram.com/b4h.mi/" target="_blank" className="social-icon">
+            <i className="fa-brands fa-instagram"></i>
+          </a>
           <a href="https://www.linkedin.com/in/batoolfahmi/" className="social-icon" target="_blank">
             <i className="fab fa-linkedin"></i>
           </a>
@@ -17,13 +17,23 @@ function Footer() {
           </a>
         </div>
 
-          <nav className="footer-nav">
-          <a href="#home">Home</a>
-          <a href="#about">About</a>
-          <a href="#blog">Blog</a>
-          <a href= "#expertise">Expertise</a>
-          <a href="#projects">Projects</a>
-          <a href="#contact">Contact</a>
+        {/* FOOTER MENY IKON */}
+        <div className="footer-menu-icon">
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+
+        {/* FOOTER MENY LISTA */}
+        <nav className="footer-menu">
+          <ul>
+            <li><a href="#home">Home</a></li>
+            <li><a href="#about">About</a></li>
+            <li><a href="#blog">Blog</a></li>
+            <li><a href="#expertise">Expertise</a></li>
+            <li><a href="#projects">Projects</a></li>
+            <li><a href="#contact">Contact</a></li>
+          </ul>
         </nav>
 
       </div>
